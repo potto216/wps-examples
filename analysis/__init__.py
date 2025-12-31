3aeae8ec85face431ca5732d35f460a610390ceb
Paul Otto <potto216@users.noreply.github.com>
2025-12-30 19:04:49 -0500

    Add GPS XML loaders and map plotting helpers

--- analysis/__init__.py
+++ analysis/__init__.py
@@ -1,1 +1,19 @@
 """Analysis helpers for WPS examples."""
+
+from .gps import (
+    GpsColumns,
+    interpolate_gps_events,
+    load_gpx,
+    load_kml,
+    plot_gps_path_map,
+    plot_gps_with_events_map,
+)
+
+__all__ = [
+    "GpsColumns",
+    "interpolate_gps_events",
+    "load_gpx",
+    "load_kml",
+    "plot_gps_path_map",
+    "plot_gps_with_events_map",
+]
